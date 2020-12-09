bbba00240ffd8aacacb761fca9db2318f35eb41a
piotrek_kp <piotrek.kp@gmail.com>
2020-12-09 12:14:11 +0100

    CRA and Express setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="main-container">
+            <div className="container-y">
+                <div className="item-y"></div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
