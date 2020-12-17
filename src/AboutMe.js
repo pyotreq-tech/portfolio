593b5905d973a8709da7c627a3b7a501297228fd
piotrek_kp <piotrek.kp@gmail.com>
2020-12-17 16:41:58 +0100

    About Me component

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,8 +7,36 @@ function AboutMe() {
                     style={{ borderRadius: "50%", maxWidth: "100%" }}
                 />
             </div>
-            <div className="half" style={{ backgroundColor: "CADETBLUE" }}>
-                fs
+            <div
+                className="half"
+                style={{
+                    padding: "15px",
+                    backgroundColor: "CADETBLUE",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <h2>Welcome</h2>
+                <h3>I am Piotr</h3>
+                <p>
+                    Full stack web developer. Privately I am interested in
+                    travelling, computer games, new technologies and modern
+                    world high-rise architecture.
+                </p>
+                <p>
+                    I am mainly focused on:
+                    <ul>
+                        <li>Javascript with HTML and CSS</li>
+                        <li>Node.js</li>
+                        <li>React.js</li>
+                        <li>PostgreSQL</li>
+                        <li>API</li>
+                        <li>GIT</li>
+                        <li>State Management (Redux)</li>
+                        <li>Socket.IO</li>
+                    </ul>
+                </p>
             </div>
         </>
     );
